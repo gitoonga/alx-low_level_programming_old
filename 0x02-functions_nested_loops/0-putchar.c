f252b002bddbcdcc378c87d522bdb60c8b16e212
Julius Macharia <mjulius05@gmail.com>
2022-12-13 15:14:20 +0300

    rogram that prints _putchar, followed by a new line.

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "customh.h"
+
+/**
+ *main - entry point to program
+ *Return: 0
+ */
+int main(void)
+{
+	int i;
+	char str[] = "_putchar";
+
+	for (i = 0; str[i] != '\0'; i++)
+		_putchar(str[i];
+	_putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/customh.h
@@ -0,0 +1,9 @@
+#ifndef _CUSTOMH_H_
+#define _CUSTOMH_H_
+
+/**
+ * _putchar - writes the character c to stdout
+ * returns 1 on Success and -1 on Error
+ */
+
+int _putchar (char c);
