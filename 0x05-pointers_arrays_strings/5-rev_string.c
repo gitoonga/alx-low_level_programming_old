46ffc6389cb131330b0e6c8d2b9cc9633246453d
Julius Macharia <mjulius05@gmail.com>
2022-12-20 01:52:03 +0300

    task 5 re imagined

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,28 +1,30 @@
 #include "main.h"
 
+
 /**
  * rev_string - reverses a string
  * @s: string to reverse
  */
 void rev_string(char *s)
 {
-	size_t len = 0;
+	int len = 0;
 	char *p = s;
 	char tmp;
 
-    /**Calculate the length of the string*/
-    while (*p != '\0')
-    {
-	len++;
-	p++;
-    }
+	/**Calculate the length of the string*/
 
-    /** Swap the characters at the beginning and end of the string*/
-    for (size_t i = 0; i < len / 2; i++)
-    {
-	tmp = s[i];
-	s[i] = s[len - i - 1];
-	s[len - i - 1] = tmp;
-    }
+	while (*p != '\0')
+	{
+		len++;
+		p++;
+	}
+	
+	/**Swap the characters at the beginning and end of the string*/
+	for (int i = 0; i < len / 2; i++)
+	{
+		tmp = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = tmp;
+	}
 }
 
