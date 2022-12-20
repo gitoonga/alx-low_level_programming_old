0e36eaab0d6225c0ba670ef193410ef4148f3685
Julius Macharia <mjulius05@gmail.com>
2022-12-20 03:31:58 +0300

    task 10'

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,43 +1,27 @@
 #include "main.h"
 
 /**
- * _atoi - Convert a string to an integer
- * @s: The string to be converted
+ * _atoi - Converts a string to an integer.
+ * @s: The string to be converted.
  *
- * The number in the string can be preceded by an
- * infinite number of characters.
- * The function takes into account all the - and + signs before the number.
- * If there are no numbers in the string, the function returns 0.
- *
- * Return: The converted integer
+ * Return: The integer value of the converted string.
  */
 int _atoi(char *s)
 {
 	int sign = 1;
-	int result = 0;
-	int i = 0;
-	int found_number = 0;
+	unsigned int num = 0;
 
-	while (s[i] != '\0')
-	{
-		if (s[i] == '-')
-		{
-			sign = -1;
-		}
-		else if (s[i] == '+')
-		{
-		}
-		else if (s[i] >= '0' && s[i] <= '9')
-		{
-			found_number = 1;
-			result = result * 10 + (s[i] - '0');
-		}
-		else if (found_number)
-		{
+	do {
+		if (*s == '-')
+			sign *= -1;
+
+		else if (*s >= '0' && *s <= '9')
+			num = (num * 10) + (*s - '0');
+
+		else if (num > 0)
 			break;
-		}
-		i++;
-	}
-	return (sign * result);
-}
 
+	} while (*s++);
+
+	return (num * sign);
+}
