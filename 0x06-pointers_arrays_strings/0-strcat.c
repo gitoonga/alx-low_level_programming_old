2c446e608d36abb166861680b364f86e2a92a039
Julius Macharia <mjulius05@gmail.com>
2022-12-22 00:59:33 +0300

    task 1

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenate two strings
+ * @dest: string to be appended to
+ * @src: string to append
+ * Return: concatenated string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int d, s = 0;
+	/**loop over the dest until you meet the null character*/
+
+	while (dest[d] != '\0')
+		d++;
+
+	while (src[s] != '\0')
+	{
+		dest[d] = src[s];
+		d++;
+		s++;
+	}
+	dest[i] = '\0';
+
+	return(dest);
+}
