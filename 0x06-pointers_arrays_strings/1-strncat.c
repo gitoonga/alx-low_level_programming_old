57fd23bd23c60add8ad920546b68054d8bfbf857
Julius Macharia <mjulius05@gmail.com>
2022-12-22 01:33:10 +0300

    task 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenate n bytes to destination string
+ * @dest: string to be appended to
+ * @src: string to append
+ * @n: append n number of bytes(chars)
+ * Return: concatenated string
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int d = 0;
+	int s = 0;
+
+	while(dest[d] != '\0')
+		d++;
+
+	while(src[s] != src[n])
+	{
+		dest[d] = src[s];
+		d++;
+		s++;
+	}
+	dest[d] = '\0';
+
+	return (dest);
+}
