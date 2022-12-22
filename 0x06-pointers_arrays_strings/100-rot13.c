55404a3a7f5ed9f42b7ad30bcce9e3bf69d1ebf4
Julius Macharia <mjulius05@gmail.com>
2022-12-22 23:10:46 +0300

    task 8

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,27 +1,36 @@
 #include "main.h"
 
 /**
- *rot13 - a simple letter substitution cipher that
- *replaces a letter with the 13th letter after it in the alphabet.
- *@s: String being encoded
- *Return: The encoded string
+ * rot13 - encode string using rot13
+ * @s: string to encode
+ * Return: encoded string
  */
 
 char *rot13(char *s)
 {
-	int i = 0;
 
-	while (s[i] != '\0')
+	int a[53] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+		     'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+		     'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+		     'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+		     'W', 'X', 'Y', 'Z'};
+	int b[53] = {'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+		     'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+		     'l', 'm', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
+		     'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+		     'J', 'K', 'L', 'M'};
+
+	int i, j;
+
+	for (j = 0; s[j] != '\0'; j++) /*loop through string*/
 	{
-		if (s[i] >= 'A' && s[i] <= 'Z')
-		{
-			s[i] = (s[i] - 'A' + 13) % 26 + 'A';
-		}
-		else if (s[i] >= 'a' && s[i] <= 'z')
-		{
-			s[i] = (s[i] - 'a' + 13) % 26 + 'a';
-		}
-		i++;
+		i = 0;
+		while (a[i] != '\0' && s[j] != a[i]) /*loop through rot13 arr*/
+			i++;
+
+		if (s[j] == a[i]) /*if alpha matches, set to index in b arr*/
+			s[j] = b[i];
 	}
+
 	return (s);
 }
