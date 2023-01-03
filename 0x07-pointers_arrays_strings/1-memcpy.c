70eac0359bb22299f13bdf25ecdcbff1e4decac6
Julius Macharia <mjulius05@gmail.com>
2023-01-03 18:42:26 +0300

    task 1 correct

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -12,11 +12,11 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	int bts = n;
 
-	if (n > 0)
+	if (bts > 0)
 	{
-		i = 0;
+		int i;
 
-		for (i > 0; i < n; i++)
+		for (i = 0; i < bts; i++)
 			dest[i] = src[i];
 	}
 
