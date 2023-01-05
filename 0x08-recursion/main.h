37403ea2e56f49744808841509ced3c6d69fda31
Julius Macharia <mjulius05@gmail.com>
2023-01-05 23:56:40 +0300

    advanced task 2

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -26,4 +26,13 @@ int compare(char *beg, char *end);
 int _strlen(char *s);
 
 int is_palindrome(char *s);
+
+int strlen_no_wilds(char *str);
+
+void iterate_wild(char **wildstr);
+
+char *postfix_match(char *str, char *postfix);
+
+int wildcmp(char *s1, char *s2);
+
 #endif
