3a081484876cdcdf0dac0e953e16a719ad0b6814
Julius Macharia <mjulius05@gmail.com>
2023-01-31 10:17:26 +0300

    fix char count on 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -7,8 +7,8 @@
 
 void printfirst(void)
 {
-	printf("You're beat! and yet, you must allow, \n");
-	printf("I bore my house upon my back! \n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
 
 void __attribute__ ((constructor)) printfirst();
