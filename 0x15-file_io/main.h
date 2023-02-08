003ac59de606b3dc10197bc0e007d31a945d3c7d
Julius Macharia <mjulius05@gmail.com>
2023-02-09 00:14:00 +0300

    Fix main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,14 +5,19 @@
 #include <stdlib.h>
 #include "string.h"
 
+/* open */
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+/* close */
+#include <unistd.h>
+
+
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-void err_read(char *s);
-static ssize_t read_content(char *source, char **buffer, int desc);
-void cp_err(char *content, char *buffer);
-static void cp(char *content, int dest, char *buffer, int length);
 
 
 #endif
